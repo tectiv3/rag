2cded68fda74552e5618feb0a1d150069b8f402a
tectiv3 <git@getpanda.me>
2024-09-29 09:05:50 +0100

    Refactor rag script

--- rag.py
+++ rag.py
@@ -1,18 +1,11 @@
-import os
 from dotenv import load_dotenv
 import faiss
-import pickle
-from langchain_openai import ChatOpenAI
-from langchain_community.document_loaders import DirectoryLoader
 from langchain_community.vectorstores import FAISS
-# from langchain.retrievers import FaissRetriever
-
+from langchain_openai import ChatOpenAI
+from langchain_openai import OpenAIEmbeddings
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_openai import OpenAIEmbeddings
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders import TextLoader
 
 load_dotenv()
 
@@ -28,32 +21,10 @@ system_prompt = (
 )
 
 prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", system_prompt),
-        ("human", "{input}"),
-    ]
+    [ ("system", system_prompt), ("human", "{input}") ]
 )
 
-# loader = DirectoryLoader("./docs", glob="**/*.md", loader_cls=TextLoader)
-# docs = loader.load()
-#
-# text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-# splits = text_splitter.split_documents(docs)
-# vectorstore = FAISS.from_documents(documents=splits, embedding=OpenAIEmbeddings())
-
-# faiss.write_index(vectorstore.index, "docs.index")
-# vectorstore.save_local("faiss_index")
-
-# vectorstore.index = None
-# with open("faiss_store.pkl", "wb") as f:
-#     pickle.dump(vectorstore, f)
-
-# index = faiss.read_index('docs.index')
-# retriever = FaissRetriever(index=index)
-
-vectorstore = FAISS.load_local(
-    "faiss_index", OpenAIEmbeddings(), allow_dangerous_deserialization=True
-)
+vectorstore = FAISS.load_local("faiss_index", OpenAIEmbeddings(), allow_dangerous_deserialization=True)
 retriever = vectorstore.as_retriever()
 
 question_answer_chain = create_stuff_documents_chain(llm, prompt)
